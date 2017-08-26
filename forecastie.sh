2cfcc8116d207a9a45aa7e8abc74973b5d0e1eb9
Bernhard Rosenkränzer <bero@lindev.ch>
2017-08-26 18:21:59 +0200

    Add initial builder script for Forecastie

---
+++ forecastie.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d forecastie ] || git clone git@github.com:FreeMobileOS/forecastie
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
+	. ${CERTS}/services/openweathermap
+	# We prefer using our own API key because there's a limited number
+	# of permitted requests using the API key per minute, and upstream
+	# Forecastie tends to get too many, causing errors.
+	sed -i -e "s,78dfe9e10dd180fadd805075dd1a10d6,$KEY," forecastie/app/src/main/res/values/keys.xml
+fi
+
+cd forecastie
+
+if [ -n "$CERTS" ]; then
+	./gradlew assembleRelease
+	$ANDROID_HOME/build-tools/26.0.0/zipalign -v -p 8 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
+	$ANDROID_HOME/build-tools/25.0.3/apksigner sign --ks ${CERTS}/aosp/fmo.jks --out app/build/outputs/apk/forecastie.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
+	cp -f app/build/outputs/apk/forecastie.apk $PRODUCT_OUT_PATH
+else
+	./gradlew assembleDebug
+fi
