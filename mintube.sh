ec906b0068c9b5365e7338d2b5c98cd7eba98bf0
Satish Patel <tosatishpatel@gmail.com>
2017-09-13 17:17:33 +0530

    mintube: removed unwanted lines
    clean up script
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- mintube.sh
+++ mintube.sh
@@ -36,11 +36,7 @@ android {
 EOF
 	fi
 	./gradlew assembleRelease
-	# Alternatively, instead of adding to build.gradle above:
-	#	$ANDROID_HOME/build-tools/26.0.0/zipalign -v -p 8 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
-	#	$ANDROID_HOME/build-tools/25.0.3/apksigner sign --ks ${CERTS}/aosp/fmo.jks --out app/build/outputs/apk/forecastie.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
-	#	cp -f app/build/outputs/apk/forecastie.apk $PRODUCT_OUT_PATH
-	cp -f app/build/outputs/apk/release.apk $PRODUCT_OUT_PATH/mintube.apk
+	cp -f app/build/outputs/apk/app-release.apk $PRODUCT_OUT_PATH/mintube.apk
 else
 	./gradlew assembleDebug
 	cp -f app/build/outputs/apk/app-debug.apk $PRODUCT_OUT_PATH/mintube-debug.apk
