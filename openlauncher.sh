f007853b63ae53224a2275ecdf79791af6cb430b
Satish Patel <tosatishpatel@gmail.com>
2017-09-27 13:08:59 +0530

    Add openlauncher
    openlauncher is open source light weight launcher.
    It has few nice features like
    - one touch memory clean
    - overlayed minibar
    - dock and desk configuration
    - fullscreen mode
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

---
+++ openlauncher.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+OUTAPK=app/build/outputs/apk/app-flavorDefault-release.apk
+MODULE=openlauncher
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d $MODULE ] || git clone git@github.com:OpenLauncherTeam/openlauncher.git --branch v0.5.3 --single-branch
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
+fi
+
+cd $MODULE
+echo "HACL>>..$MODULE"
+
+if [ -n "$CERTS" ]; then
+	P="$(cat $CERTS/aosp/password)"
+	if ! grep -q fmo.jks app/build.gradle; then
+		cat >>app/build.gradle <<EOF
+android {
+	signingConfigs {
+		release {
+			storeFile file("$CERTS/aosp/fmo.jks")
+			storePassword "$P"
+			keyAlias "apps"
+			keyPassword "$P"
+		}
+	}
+	buildTypes {
+		release {
+			signingConfig signingConfigs.release
+		}
+	}
+}
+EOF
+	fi
+	./gradlew clean assembleRelease
+	cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
+else
+    echo "WARNING: Debug build is not supported"
+fi
