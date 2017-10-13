c93d0fc481f653affc81a831236a02578025540f
Satish Patel <tosatishpatel@gmail.com>
2017-10-13 12:39:16 +0530

    Add leafpic
    LeafPic is a fluid, material-designed alternative gallery, it also is ad-free
    and open source under GPLv3 license.
    
    it has many options to configure look and feel of app, including color themes.
    
    Active development is going on as per the
    https://github.com/HoraApps/LeafPic
    
    but for stability tag#v0.6-beta-1 has been taken into account
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

---
+++ leafpic.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+OUTAPK=app/build/outputs/apk/app-noGPlay-release.apk
+MODULE=leafpic
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d $MODULE ] || git clone git@github.com:HoraApps/LeafPic.git $MODULE --branch v0.6-beta-1 --single-branch
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
+fi
+
+cd $MODULE
+echo "HACL>>..$MODULE"
+
+if [ -n "$CERTS" ]; then
+	P="$(cat $CERTS/aosp/password)"
+	if ! grep -q fmo.jks app/build.gradle; then
+		cat >>app/build.gradle <<EOF
+android {
+	signingConfigs {
+		release {
+			storeFile file("$CERTS/aosp/fmo.jks")
+			storePassword "$P"
+			keyAlias "apps"
+			keyPassword "$P"
+		}
+	}
+	buildTypes {
+		release {
+			signingConfig signingConfigs.release
+		}
+	}
+}
+EOF
+	fi
+	./gradlew clean assembleRelease
+	cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
+else
+    echo "WARNING: Debug build is not supported"
+fi
