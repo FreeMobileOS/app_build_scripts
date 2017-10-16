f0f4f14f5904cec4fbe98c93821afafb46d669f4
Satish Patel <tosatishpatel@gmail.com>
2017-10-16 13:07:38 +0530

    Add silence messenger
    Silence (formerly SMSSecure) is an SMS/MMS application that allows you to
    protect your privacy while communicating with friends.
    
    Silence communicates using encrypted SMS messages. No servers or internet
    connection required.
    
    Code Ref# https://github.com/SilenceIM/Silence
    Tag for build# v0.16.4-unstable
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

---
+++ silence.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+OUTAPK=build/outputs/apk/silence-release.apk
+MODULE=silence
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d $MODULE ] || git clone git@github.com:SilenceIM/Silence.git $MODULE --branch v0.16.4-unstable --single-branch
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
+fi
+
+cd $MODULE
+# to create default configuration for silence
+git submodule init
+git submodule update
+
+echo "Preparing>>..$MODULE"
+
+if [ -n "$CERTS" ]; then
+	P="$(cat $CERTS/aosp/password)"
+	if ! grep -q fmo.jks build.gradle; then
+		cat >>build.gradle <<EOF
+android {
+	signingConfigs {
+		release {
+			storeFile file("$CERTS/aosp/fmo.jks")
+			storePassword "$P"
+			keyAlias "apps"
+			keyPassword "$P"
+		}
+	}
+	buildTypes {
+		release {
+			signingConfig signingConfigs.release
+		}
+	}
+}
+EOF
+	fi
+	./gradlew clean assembleRelease
+	cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
+else
+    echo "WARNING: Debug build is not supported"
+fi
