e087f9971382bd5bc2fc31270fce58b0d4b78c73
Bernhard Rosenkraenzer <bero@lindev.ch>
2017-10-18 21:27:03 +0200

    Add lawnchair launcher
    Signed-off-by: Bernhard Rosenkraenzer <bero@lindev.ch>

---
+++ lawnchair.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+OUTAPK=app/build/outputs/apk/release/app-release.apk
+MODULE=Lawnchair
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d $MODULE ] || git clone git@github.com:Deletescape-Media/$MODULE -b alpha --single-branch
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
+fi
+
+cd $MODULE
+# Permissions seem to be messed up in upstream's git repository
+chmod +x gradlew
+if [ -n "$CERTS" ]; then
+	P="$(cat $CERTS/aosp/password)"
+	if ! grep -q fmo.jks app/build.gradle; then
+		cat >>app/build.gradle <<EOF
+android {
+	signingConfigs {
+		release {
+			storeFile file("$CERTS/aosp/fmo.jks")
+			storePassword "$P"
+			keyAlias "apps"
+			keyPassword "$P"
+		}
+	}
+	buildTypes {
+		release {
+			signingConfig signingConfigs.release
+		}
+	}
+}
+EOF
+	fi
+	./gradlew clean assembleRelease
+	cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
+else
+	./gradlew clean assembleDebug
+	cp -f app/build/outputs/apk/debug/app-debug.apk $PRODUCT_OUT_PATH/$MODULE.apk
+fi
