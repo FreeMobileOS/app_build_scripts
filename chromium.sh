6e31b87cecfd97ffea318957737ae04ae6f644e9
Bernhard Rosenkraenzer <bero@lindev.ch>
2017-11-04 00:09:59 +0100

    Allow building chromium branches other than master

--- chromium.sh
+++ chromium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # For a list of current Android compatible versions, see
 # http://omahaproxy.appspot.com/
-GN_ARGS='target_os="android" target_cpu="arm64" is_debug=false is_official_build=true is_chrome_branded=false enable_resource_whitelist_generation=true ffmpeg_branding="ChromeOS" proprietary_codecs=true enable_remoting=true'
+GN_ARGS='target_os="android" target_cpu="arm64" is_debug=false is_official_build=true is_chrome_branded=false enable_resource_whitelist_generation=true ffmpeg_branding="Chrome" proprietary_codecs=true enable_remoting=true'
 # FIXME should probably switch to
 # GN_ARGS='target_os="android" target_cpu="arm64" proprietary_codecs=true ffmpeg_branding="ChromeOS" enable_hevc_demuxing=true'
 # to get more codecs supported... But this causes ffmpeg build breakages without patching the code
@@ -9,6 +9,8 @@ GN_ARGS='target_os="android" target_cpu="arm64" is_debug=false is_official_build
 # Monochrome is supposed to be a combined Chromium and Webview APK. Current tests show it not
 # actually replacing the system webview though.
 [ -z "$USE_MONOCHROME" ] && USE_MONOCHROME=false
+# Available channels: head, canary, dev, beta, stable
+[ -z "$CHANNEL" ] && CHANNEL=head
 
 MYDIR="$(dirname $(realpath $0))"
 cd $MYDIR
@@ -20,17 +22,49 @@ unset NEED_NDK
 unset NEED_SRC
 unset NEED_ROOTPATH
 
+set -x
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=/opt/legacy:$PATH:$(pwd)/depot_tools
 mkdir chromium
 cd chromium
-fetch --nohooks --no-history android
+# May want to add --no-history -- will that break branches?
+fetch --nohooks android
 cd src
 echo "target_os = [ 'android' ]" >>../.gclient
-gclient sync --with_branch_heads
-gclient runhooks
-# Version numbers from http://omahaproxy.appspot.com/
-#git checkout -b stable tags/61.0.3163.98
+if [ "$CHANNEL" = "head" ]; then
+	echo Using Chromium HEAD...
+	gclient sync
+	gclient runhooks
+else
+	VERSIONS=$(wget -O - "https://omahaproxy.appspot.com/all?os=android&channel=$CHANNEL" |tail -n1)
+	# VERSIONS should now be a CSV saying something like
+	# android,beta,62.0.3202.52,62.0.3202.45,10/12/17,10/05/17,fa6a5d87adff761bc16afc5498c3f5944c1daa68,499098,4f1faed364af4797f7a86a908b85b369d9892539,3202,6.2.414.30
+	# Meaning:
+	# 1 os
+	# 2 channel
+	# 3 current_version
+	# 4 previous_version
+	# 5 current_reldate
+	# 6 previous_reldate
+	# 7 branch_base_commit
+	# 8 branch_base_position
+	# 9 branch_commit
+	# 10 true_branch
+	# 11 v8_version
+	CURRENT_VERSION=$(echo ${VERSIONS} |cut -d, -f3)
+	echo Using Chromium ${CURRENT_VERSION}...
+	COMMIT=$(echo ${VERSIONS} |cut -d, -f9)
+	gclient sync -r ${COMMIT}
+	gclient runhooks
+fi
+
+# As of AOSP O 8.0.0_r17, the bundled system WebView's versionCode is 303012550
+# We need to outnumber that if we want to install an update...
+# We should also outnumber our own previous builds, so let's put a date code in there.
+# picking 379828897 as a base because it's sufficiently larger than 303012550 and it
+# will give our first build, done on 2017/11/03, a nice round number.
+VERCODE=$((379828897+$(date +%Y%m%d)))
+find . -name AndroidManifest.xml |xargs sed -i -e "s,android:versionCode=\"1\",android:versionCode=\"$VERCODE\",g"
 
 gn gen --args="${GN_ARGS}" out/Release
 if $USE_MONOCHROME; then
