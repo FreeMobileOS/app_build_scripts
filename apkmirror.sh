4e2500fd9844b70b6a0d60e1a63d28a9b533cb6d
Satish Patel <tosatishpatel@gmail.com>
2017-11-10 17:55:51 +0530

    Add apkmirror
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

---
+++ apkmirror.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+OUTAPK=build/outputs/apk/app-release.apk
+MODULE=apkmirror
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d $MODULE ] || git clone git@github.com:FreeMobileOS/APKMirror.git $MODULE --branch fmo-3.3.4 --single-branch
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
+fi
+
+cd $MODULE/app
+
+echo "Preparing>>..$MODULE"
+
+if [ -n "$CERTS" ]; then
+	P="$(cat $CERTS/aosp/password)"
+	if ! grep -q fmo.jks build.gradle; then
+		cat >>build.gradle <<EOF
+android {
+	signingConfigs {
+		release {
+			storeFile file("$CERTS/aosp/fmo.jks")
+			storePassword "$P"
+			keyAlias "apps"
+			keyPassword "$P"
+		}
+	}
+	buildTypes {
+		release {
+			signingConfig signingConfigs.release
+		}
+	}
+}
+EOF
+	fi
+	./gradlew clean assembleRelease
+	cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
+else
+    echo "WARNING: Debug build is not supported"
+fi
