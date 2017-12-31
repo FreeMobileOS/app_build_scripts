aa58d229d3c3b81d3f0be65f238abb0d8b313750
Bernhard Rosenkränzer <bero@lindev.ch>
2017-12-31 03:27:46 +0100

    Add open-keychain builder, this will supplement K9-Mail nicely
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

---
+++ open-keychain.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+[ -z "$VERSION" ] && VERSION=v4.8.1
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d open-keychain ] || git clone git@github.com:open-keychain/open-keychain --branch $VERSION --single-branch --depth 1
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
+fi
+
+cd open-keychain
+git submodule update --init --recursive
+
+if [ -n "$CERTS" ]; then
+	P="$(cat $CERTS/aosp/password)"
+	if ! grep -q fmo.jks OpenKeychain/build.gradle; then
+		cat >>OpenKeychain/build.gradle <<EOF
+android {
+	signingConfigs {
+		release {
+			storeFile file("$CERTS/aosp/fmo.jks")
+			storePassword "$P"
+			keyAlias "apps"
+			keyPassword "$P"
+		}
+	}
+	buildTypes {
+		release {
+			signingConfig signingConfigs.release
+		}
+	}
+}
+EOF
+	fi
+	./gradlew assembleRelease
+	# Alternatively, instead of adding to build.gradle above:
+	#	$ANDROID_HOME/build-tools/26.0.0/zipalign -v -p 8 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
+	#	$ANDROID_HOME/build-tools/25.0.3/apksigner sign --ks ${CERTS}/aosp/fmo.jks --out app/build/outputs/apk/forecastie.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
+	#	cp -f app/build/outputs/apk/forecastie.apk $PRODUCT_OUT_PATH
+	cp -f OpenKeychain/build/outputs/apk/OpenKeychain-fdroid-release.apk $PRODUCT_OUT_PATH/open-keychain.apk
+else
+	./gradlew assembleDebug
+	cp -f OpenKeychain/build/outputs/apk/OpenKeychain-fdroid-debug.apk $PRODUCT_OUT_PATH/open-keychain.apk
+fi
