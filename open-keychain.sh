a4d9ca75b65e76d6bfc1e6835295609d33088ab0
Bernhard Rosenkränzer <bero@lindev.ch>
2018-02-03 20:09:31 +0100

    OpenKeychain 4.9-beta1, Rename to Encryption
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- open-keychain.sh
+++ open-keychain.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MYDIR="$(dirname $(realpath $0))"
-[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+. ${MYDIR}/envsetup.sh
 [ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
-[ -z "$VERSION" ] && VERSION=v4.8.1
+[ -z "$VERSION" ] && VERSION=v4.9-beta1
 
 mkdir -p "$APP_ROOT_PATH"
 cd "$APP_ROOT_PATH"
@@ -14,6 +14,8 @@ fi
 
 cd open-keychain
 git submodule update --init --recursive
+
+autoTranslate OpenKeychain/src/main/res/values/strings.xml app_name "Encryption"
 
 if [ -n "$CERTS" ]; then
 	P="$(cat $CERTS/aosp/password)"
@@ -41,7 +43,7 @@ EOF
 	#	$ANDROID_HOME/build-tools/26.0.0/zipalign -v -p 8 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
 	#	$ANDROID_HOME/build-tools/25.0.3/apksigner sign --ks ${CERTS}/aosp/fmo.jks --out app/build/outputs/apk/forecastie.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
 	#	cp -f app/build/outputs/apk/forecastie.apk $PRODUCT_OUT_PATH
-	cp -f OpenKeychain/build/outputs/apk/OpenKeychain-fdroid-release.apk $PRODUCT_OUT_PATH/open-keychain.apk
+	cp -f OpenKeychain/build/outputs/apk/fdroid/release/OpenKeychain-fdroid-release.apk $PRODUCT_OUT_PATH/open-keychain.apk
 else
 	./gradlew assembleDebug
 	cp -f OpenKeychain/build/outputs/apk/OpenKeychain-fdroid-debug.apk $PRODUCT_OUT_PATH/open-keychain.apk
