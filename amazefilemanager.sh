61e805e287ed2b7c56d166b80538ffbf2944fb21
Satish Patel <tosatishpatel@gmail.com>
2018-04-18 17:37:15 +0530

    Add amaze filemanager
    Provide support for local file management + package install
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

---
+++ amazefilemanager.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+MYDIR="$(dirname $(realpath $0))"
+OUTAPK=app/build/outputs/apk/app-fdroid-release.apk
+MODULE=amazefilemanager
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+[ -z "$VERSION" ] && VERSION=v.3.2.1
+
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d $MODULE ] || git clone git@github.com:TeamAmaze/AmazeFileManager.git $MODULE --branch $VERSION --single-branch
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+        CERTS="$(pwd)"/secret-keys
+fi
+
+cd $MODULE
+echo "HACL>>..$MODULE"
+
+if [ -n "$CERTS" ]; then
+        P="$(cat $CERTS/aosp/password)"
+        if ! grep -q fmo.jks app/build.gradle; then
+                cat >>app/build.gradle <<EOF
+android {
+        signingConfigs {
+                release {
+                        storeFile file("$CERTS/aosp/fmo.jks")
+                        storePassword "$P"
+                        keyAlias "apps"
+                        keyPassword "$P"
+                }
+        }
+        buildTypes {
+                release {
+                        signingConfig signingConfigs.release
+                }
+        }
+}
+EOF
+        fi
+        ./gradlew clean assembleRelease
+        cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
+else
+    echo "Warning: Debug build is not supported"
+fi
