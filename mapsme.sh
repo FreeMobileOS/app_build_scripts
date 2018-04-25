3044b4b37755e5ca1059e3f14d1ceed3e5c2ad76
Satish Patel <tosatishpatel@gmail.com>
2018-04-25 11:00:36 +0530

    omim: Updated script to rel-82 stable release
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- mapsme.sh
+++ mapsme.sh
@@ -1,31 +1,37 @@
 #!/bin/sh
 MYDIR="$(dirname $(realpath $0))"
-. ${MYDIR}/envsetup.sh
+OUTAPK=app/build/outputs/apk/release/app-release.apk
+MODULE=omim
+[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
+[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+[ -z "$VERSION" ] && VERSION=fmo-1.1.0.1742
 
-if [ ! -d omim ]; then
-	git clone --depth 1 -b fmo-release-81 git@github.com:FreeMobileOS/omim
-	cd omim
-	git submodule init
-	git submodule update
-	cd ..
+mkdir -p "$APP_ROOT_PATH"
+cd "$APP_ROOT_PATH"
+[ -d $MODULE ] || git clone git@github.com:FreeMobileOS/$MODULE --depth 1 -b $VERSION --single-branch
+[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
+if [ -d secret-keys ]; then
+	CERTS="$(pwd)"/secret-keys
 fi
 
-cd omim
+cd $MODULE
+git submodule init
+git submodule update
+
 export NDK_ABI=arm64-v8a
 echo | ./configure.sh
-./tools/android/set_up_android.py --sdk ~/Android/Sdk --ndk ~/Android/Sdk/ndk-bundle
+./tools/android/set_up_android.py --sdk $ANDROID_HOME --ndk $ANDROID_NDK_PATH
 sed -i -e 's,#define DEFAULT_URLS_JSON.*,#define DEFAULT_URLS_JSON \"[\\"http://direct.mapswithme.com/\\" ]\",' private.h
 sed -i -e 's|cppFlags .*|&, "-DANDROID64"|' android/build.gradle
 sed -i -e 's|cFlags .*|&, "-DANDROID64"|' android/build.gradle
+
+# Note: To fix build error for release-82
+echo "#define GOOGLE_WEB_CLIENT_ID \"\" " >> private.h
 cd android
 
 # Use generic name
 autoTranslate res/values/strings.xml app_name Maps
 
-[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
-if [ -d secret-keys ]; then
-	CERTS="$(pwd)"/secret-keys
-fi
 if [ -n "$CERTS" ]; then
 	P="$(cat $CERTS/aosp/password)"
 	if ! grep -q fmo.jks app/build.gradle; then
@@ -49,5 +55,5 @@ EOF
 	fi
 fi
 
-./gradlew -Parm64=1 -Darm64=1 clean assembleWebRelease
-cp build/outputs/apk/android-web-release-*-*.apk ../../out/mapsme.apk
+./gradlew -Parm64=1 -Darm64=1 assembleWebRelease
+cp build/outputs/apk/android-web-release-*-*.apk $PRODUCT_OUT_PATH/mapsme.apk
