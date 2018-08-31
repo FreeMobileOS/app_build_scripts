9e4dbec8e46d500857cbafd01ec9de886ed3e861
Satish Patel <tosatishpatel@gmail.com>
2018-08-31 17:44:15 +0530

    fdroidclient: Moved to release 1.4-alpha1
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- fdroidclient.sh
+++ fdroidclient.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 MYDIR="$(dirname $(realpath $0))"
-OUTAPK=./app/build/outputs/apk/app-release.apk
+OUTAPK=./app/build/outputs/apk/full/release/app-full-release.apk
 MODULE=fdroidclient
 [ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
 [ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
 
 mkdir -p "$APP_ROOT_PATH"
 cd "$APP_ROOT_PATH"
-[ -d $MODULE ] || git clone https://gitlab.com/fdroid/fdroidclient.git --branch 1.1-alpha2 --single-branch
+[ -d $MODULE ] || git clone https://gitlab.com/fdroid/fdroidclient.git --branch 1.4-alpha1 --single-branch
 [ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
 if [ -d secret-keys ]; then
 	CERTS="$(pwd)"/secret-keys
 fi
 
 cd $MODULE
-echo "HACL>>..$MODULE"
+echo "LOG>>..$MODULE"
 
 if [ -n "$CERTS" ]; then
 	P="$(cat $CERTS/aosp/password)"
