727617453fca0ad1b659f9aa2c27735bb2a71137
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-22 18:12:24 +0200

    Update and fix antennapod
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- antennapod.sh
+++ antennapod.sh
@@ -2,7 +2,7 @@
 MYDIR="$(dirname $(realpath $0))"
 . ${MYDIR}/envsetup.sh
 [ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
-[ -z "$VERSION" ] && VERSION="1.6.4.5"
+[ -z "$VERSION" ] && VERSION="2.5.1"
 
 mkdir -p "$APP_ROOT_PATH"
 cd "$APP_ROOT_PATH"
@@ -18,6 +18,8 @@ cd AntennaPod
 sed -i -e 's,name="app_name" translate="false",name="app_name",g' core/src/main/res/values/strings.xml app/src/main/res/values/strings.xml
 autoTranslate core/src/main/res/values/strings.xml app_name Podcasts
 autoTranslate app/src/main/res/values/strings.xml app_name Podcasts
+
+export JAVA_HOME=/usr/lib/jvm/java-14-openjdk
 
 if [ -n "$CERTS" ]; then
 	P="$(cat $CERTS/aosp/password)"
@@ -45,8 +47,8 @@ EOF
 	#	$ANDROID_HOME/build-tools/26.0.0/zipalign -v -p 8 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
 	#	$ANDROID_HOME/build-tools/25.0.3/apksigner sign --ks ${CERTS}/aosp/fmo.jks --out app/build/outputs/apk/AntennaPod.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
 	#	cp -f app/build/outputs/apk/AntennaPod.apk $PRODUCT_OUT_PATH
-	cp -f app/build/outputs/apk/app-free-release.apk $PRODUCT_OUT_PATH/AntennaPod.apk
+	cp -f app/build/outputs/apk/free/release/app-free-release.apk $PRODUCT_OUT_PATH/AntennaPod.apk
 else
 	./gradlew assembleDebug
-	cp -f app/build/outputs/apk/app-free-debug.apk $PRODUCT_OUT_PATH/AntennaPod.apk
+	cp -f app/build/outputs/apk/free/debug/app-free-debug.apk $PRODUCT_OUT_PATH/AntennaPod.apk
 fi
