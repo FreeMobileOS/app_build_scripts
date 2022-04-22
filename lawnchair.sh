7b3f5fc547312c9e02bd04750c57df0aed1389b4
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-22 04:13:25 +0200

    Allow building current lawnchair

--- lawnchair.sh
+++ lawnchair.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 MYDIR="$(dirname $(realpath $0))"
-OUTAPK=./build/outputs/apk/quickstepLawnchairPlah/release/Lawnchair-quickstep-lawnchair-plah-release.apk
-MODULE=Lawnchair
+MODULE=lawnchair
+export NEED_SRC=false
 [ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
 [ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
-[ -z "$VERSION" ] && VERSION=fmo-alpha
+[ -z "$VERSION" ] && VERSION=12-dev
 
 mkdir -p "$APP_ROOT_PATH"
 cd "$APP_ROOT_PATH"
-[ -d $MODULE ] || git clone git@github.com:FreeMobileOS/$MODULE -b $VERSION --single-branch
+[ -d $MODULE ] || git clone --recursive git@github.com:LawnchairLauncher/$MODULE -b $VERSION --single-branch
 [ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
 if [ -d secret-keys ]; then
 	CERTS="$(pwd)"/secret-keys
 fi
 
 cd $MODULE
+git submodule update --init --recursive .
 # Permissions seem to be messed up in upstream's git repository
 chmod +x gradlew
 # change targetSdkVersion to fix crash error on 8.1.0
-# T_OLD="argetSdkVersion 27"
-# T_NEW="argetSdkVersion 22"
-# sed -i "s/$T_OLD/$T_NEW/g" app/build.gradle
+#T_OLD="argetSdkVersion 27"
+#T_NEW="argetSdkVersion 22"
+#sed -i "s/$T_OLD/$T_NEW/g" app/build.gradle
 
 # require to package with sign other than travis
 export TRAVIS_EVENT_TYPE="pull_request"
@@ -46,9 +47,9 @@ android {
 }
 EOF
 	fi
-    ./gradlew clean assembleQuickstepLawnchairPlah
-	cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
+	./gradlew clean assembleLawnWithQuickstepRelease
+	cp -f ./build/outputs/apk/lawnWithQuickstep/release/Lawnchair*.apk $PRODUCT_OUT_PATH/$MODULE.apk
 else
-	./gradlew clean assembleDebug
-	cp -f app/build/outputs/apk/debug/app-debug.apk $PRODUCT_OUT_PATH/$MODULE.apk
+	./gradlew clean assembleLawnWithQuickstepDebug
+	cp -f ./build/outputs/apk/lawnWithQuickstep/debug/Lawnchair*.apk $PRODUCT_OUT_PATH/$MODULE.apk
 fi
