73e4a441ce5ed10e13d1e7b9e3aba13e93384ef5
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-22 18:13:00 +0200

    Update and fix PassAndroid
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- passandroid.sh
+++ passandroid.sh
@@ -2,7 +2,7 @@
 MYDIR="$(dirname $(realpath $0))"
 [ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
 [ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
-[ -z "$VERSION" ] && VERSION=3.4.5
+[ -z "$VERSION" ] && VERSION=3.5.7
 
 mkdir -p "$APP_ROOT_PATH"
 cd "$APP_ROOT_PATH"
@@ -15,10 +15,16 @@ fi
 cd PassAndroid
 autoTranslate android/src/main/res/values/strings.xml app_name Wallet
 
+# FIXME use of CMSClassUnloadingEnabled
+export JAVA_HOME=/usr/lib/jvm/java-14-openjdk
+
 if [ -n "$CERTS" ]; then
 	P="$(cat $CERTS/aosp/password)"
 
+	# "Superfluous" newline at the beginning is necessary
+	# since upstream likes to end files without EOL
 	cat >>android/build.gradle <<EOF
+
 android {
 	signingConfigs {
 		release {
@@ -42,8 +48,8 @@ KEY_ALIAS=apps
 KEY_PASSWORD=$P
 EOF
 	./gradlew assembleRelease
-	cp -f android/build/outputs/apk/PassAndroid-*-noMaps-noAnalytics-forFDroid-release.apk $PRODUCT_OUT_PATH/PassAndroid.apk
+	cp -f android/build/outputs/apk/noMapsNoAnalyticsForFDroid/release/PassAndroid-*-noMaps-noAnalytics-forFDroid-release.apk $PRODUCT_OUT_PATH/PassAndroid.apk
 else
 	./gradlew assembleDebug
-	cp -f android/build/outputs/apk/PassAndroid-*-noMaps-noAnalytics-forFDroid-release.apk $PRODUCT_OUT_PATH/PassAndroid.apk
+	cp -f android/build/outputs/apk/noMapsNoAnalyticsForFDroid/release/PassAndroid-*-noMaps-noAnalytics-forFDroid-release.apk $PRODUCT_OUT_PATH/PassAndroid.apk
 fi
