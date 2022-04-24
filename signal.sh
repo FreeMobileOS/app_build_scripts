fda255d291ca2f68916230376c44a044e52a26c6
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-24 21:28:42 +0200

    Clean up signal
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- signal.sh
+++ signal.sh
@@ -1,68 +1,13 @@
 #!/bin/sh
+[ -z "$VERSION" ] && VERSION=v5.36.2
+SOURCE=git@github.com:signalapp/Signal-Android
 MYDIR="$(dirname $(realpath $0))"
-[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
-[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
+. ${MYDIR}/envsetup.sh
 
-VERSION=5.36.2
+checkout
+force_java_version 14
 
-mkdir -p "$APP_ROOT_PATH"
-cd "$APP_ROOT_PATH"
-[ -d Signal-Android ] || git clone --depth 1 -b v${VERSION} git@github.com:signalapp/Signal-Android
-[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
-if [ -d secret-keys ]; then
-	CERTS="$(pwd)"/secret-keys
-fi
-
-export JAVA_HOME=/usr/lib/jvm/java-14-openjdk
-
-cd Signal-Android
-
-# let's built signal to avoid gcm communication
-sed -i -e 's,PlayServicesUtil.getPlayServicesStatus(this);,PlayServicesStatus.MISSING;,' src/org/thoughtcrime/securesms/RegistrationActivity.java
-
-# let's fix expiry period
-sed -i -e 's,return 90 - age;,return 390 - age;,' src/org/thoughtcrime/securesms/util/Util.java
-
-# let's do not show update notification
-sed -i -e '/UpdateApkRefreshListener.schedule/d' src/org/thoughtcrime/securesms/ApplicationContext.java
-
-# fix sign build. We are using our signature
-sed -i -e '/task.finalizedBy signProductionPlayRelease/d' build.gradle
-sed -i -e '/task.finalizedBy signProductionWebsiteRelease/d' build.gradle
-
-sed -i -e 's,app_name">Signal,app_name">Instant Messenger,' res/values/strings.xml
-
-# add applicationId to resolve conflicts with playstore version.
-if ! grep -q com.fmo.signal build.gradle; then
-    sed -i '/'$VERSION'/a \\tapplicationId "com.fmo.signal"' build.gradle
-fi
-
-if [ -n "$CERTS" ]; then
-        P="$(cat $CERTS/aosp/password)"
-        if ! grep -q fmo.jks app/build.gradle; then
-                cat >>app/build.gradle <<EOF
-android {
-        signingConfigs {
-                release {
-                        storeFile file("$CERTS/aosp/fmo.jks")
-                        storePassword "$P"
-                        keyAlias "apps"
-                        keyPassword "$P"
-                }
-        }
-        buildTypes {
-                release {
-                        signingConfig signingConfigs.release
-                }
-        }
-}
-EOF
-	fi
-	echo "release build"
-	./gradlew assembleRelease
-	cp -f /tmp/appbuild.sxDbIK/Signal-Android/app/build/outputs/apk/websiteProd/release/Signal-Android-website-prod-arm64-v8a-release-unsigned-*.apk $PRODUCT_OUT_PATH/signal.apk
-else
-	echo "debug build"
-	./gradlew assembleDebug
-	cp -f build/outputs/apk/website/release/Signal-website-debug-$VERSION.apk $PRODUCT_OUT_PATH/signal.apk
-fi
+add_certs_to_gradle app/build.gradle
+./gradlew assembleRelease
+output app/build/outputs/apk/websiteProd/release/Signal-Android-website-prod-arm64-v8a-release-*.apk
+cleanup
