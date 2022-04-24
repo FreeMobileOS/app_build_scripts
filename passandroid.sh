4ddee5a4f6d3d418489f2f92e3a3c05075d3196a
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-24 20:12:47 +0200

    Clean up PassAndroid, allow maps feature with microG
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- passandroid.sh
+++ passandroid.sh
@@ -1,55 +1,23 @@
 #!/bin/sh
-MYDIR="$(dirname $(realpath $0))"
-[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
-[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
 [ -z "$VERSION" ] && VERSION=3.5.7
+SOURCE=git@github.com:ligi/PassAndroid
+MYDIR="$(dirname $(realpath $0))"
+. ${MYDIR}/envsetup.sh
 
-mkdir -p "$APP_ROOT_PATH"
-cd "$APP_ROOT_PATH"
-[ -d PassAndroid ] || git clone --depth 1 -b ${VERSION} git@github.com:ligi/PassAndroid
-[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
-if [ -d secret-keys ]; then
-	CERTS="$(pwd)"/secret-keys
-fi
+checkout
 
-cd PassAndroid
 autoTranslate android/src/main/res/values/strings.xml app_name Wallet
 
 # FIXME use of CMSClassUnloadingEnabled
-export JAVA_HOME=/usr/lib/jvm/java-14-openjdk
+force_java_version 14
 
-if [ -n "$CERTS" ]; then
-	P="$(cat $CERTS/aosp/password)"
+echo >>android/build.gradle # ****ing missing newline at end of file
+add_certs_to_gradle android/build.gradle
 
-	# "Superfluous" newline at the beginning is necessary
-	# since upstream likes to end files without EOL
-	cat >>android/build.gradle <<EOF
+# Allow the combination of Free and withMaps -- thanks, microG!
+sed -i -e "s,|| (distribution == 'forFDroid' && maps == 'withMaps'),," android/build.gradle
 
-android {
-	signingConfigs {
-		release {
-			storeFile file("$CERTS/aosp/fmo.jks")
-			storePassword "$P"
-			keyAlias "apps"
-			keyPassword "$P"
-		}
-	}
-	buildTypes {
-		release {
-			signingConfig signingConfigs.release
-		}
-	}
-}
-EOF
-	cat >signing.properties <<EOF
-STORE_FILE=$CERTS/aosp/fmo.jks
-STORE_PASSWORD=$P
-KEY_ALIAS=apps
-KEY_PASSWORD=$P
-EOF
-	./gradlew assembleRelease
-	cp -f android/build/outputs/apk/noMapsNoAnalyticsForFDroid/release/PassAndroid-*-noMaps-noAnalytics-forFDroid-release.apk $PRODUCT_OUT_PATH/PassAndroid.apk
-else
-	./gradlew assembleDebug
-	cp -f android/build/outputs/apk/noMapsNoAnalyticsForFDroid/release/PassAndroid-*-noMaps-noAnalytics-forFDroid-release.apk $PRODUCT_OUT_PATH/PassAndroid.apk
-fi
+./gradlew assembleRelease
+
+output android/build/outputs/apk/withMapsNoAnalyticsForFDroid/release/PassAndroid-*-withMaps-noAnalytics-forFDroid-release.apk
+cleanup
