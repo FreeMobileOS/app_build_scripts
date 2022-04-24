baf5d862122c1af9d912a6e797974bd43d7e51c0
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-24 20:13:02 +0200

    Clean up open-keychain
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- open-keychain.sh
+++ open-keychain.sh
@@ -1,55 +1,26 @@
 #!/bin/sh
-MYDIR="$(dirname $(realpath $0))"
-export NEED_SRC=false
-. ${MYDIR}/envsetup.sh
-[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
 [ -z "$VERSION" ] && VERSION=v5.7.5
+SOURCE=git@github.com:open-keychain/open-keychain
+MYDIR="$(dirname $(realpath $0))"
+. ${MYDIR}/envsetup.sh
 
-mkdir -p "$APP_ROOT_PATH"
-cd "$APP_ROOT_PATH"
-[ -d open-keychain ] || git clone git@github.com:open-keychain/open-keychain --branch $VERSION --single-branch --depth 1
-[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
-if [ -d secret-keys ]; then
-	CERTS="$(pwd)"/secret-keys
-fi
-
-cd open-keychain
-git submodule update --init --recursive
-
+checkout
 autoTranslate OpenKeychain/src/main/res/values/strings.xml app_name "Encryption"
 
 # FIXME stop forcing old javac when gradle's copy of groovyjarjarasm starts
 # supporting something newer
-export JAVA_HOME=/usr/lib/jvm/java-14-openjdk
+force_java_version 14
 
-if [ -n "$CERTS" ]; then
-	P="$(cat $CERTS/aosp/password)"
-	if ! grep -q fmo.jks OpenKeychain/build.gradle; then
-		cat >>OpenKeychain/build.gradle <<EOF
-android {
-	signingConfigs {
-		release {
-			storeFile file("$CERTS/aosp/fmo.jks")
-			storePassword "$P"
-			keyAlias "apps"
-			keyPassword "$P"
-		}
-	}
-	buildTypes {
-		release {
-			signingConfig signingConfigs.release
-		}
-	}
-}
+cat >>gradle.properties <<EOF
+
+# The extra newline above is required because upstream doesn't have an EOL
+# at the end of the original file. Don't remove it.
+signingStoreLocation=$CERT_STORE
+signingStorePassword=$CERT_PW
+signingKeyAlias=apps
+signingKeyPassword=$CERT_PW
 EOF
-	fi
-	./gradlew assembleRelease
-	# Alternatively, instead of adding to build.gradle above:
-	#	$ANDROID_HOME/build-tools/26.0.0/zipalign -v -p 8 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
-	#	$ANDROID_HOME/build-tools/25.0.3/apksigner sign --ks ${CERTS}/aosp/fmo.jks --out app/build/outputs/apk/forecastie.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
-	#	cp -f app/build/outputs/apk/forecastie.apk $PRODUCT_OUT_PATH
-	cp -f OpenKeychain/build/outputs/apk/fdroid/release/OpenKeychain-fdroid-release.apk $PRODUCT_OUT_PATH/open-keychain.apk
-else
-	./gradlew assembleDebug
-	cp -f OpenKeychain/build/outputs/apk/OpenKeychain-fdroid-debug.apk $PRODUCT_OUT_PATH/open-keychain.apk
-fi
+
+./gradlew assembleRelease
+output OpenKeychain/build/outputs/apk/fdroid/release/OpenKeychain-fdroid-release.apk
+cleanup
