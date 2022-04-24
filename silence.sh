400a375c902f2f9e23cd02e65b742ddfe6fa98c6
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-24 21:05:02 +0200

    Clean up silence
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- silence.sh
+++ silence.sh
@@ -1,52 +1,20 @@
 #!/bin/sh
-MYDIR="$(dirname $(realpath $0))"
-OUTAPK=build/outputs/apk/release/silence-release.apk
-MODULE=silence
-[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
-[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
 [ -z "$VERSION" ] && VERSION=v0.16.12-unstable
+SOURCE=https://git.silence.dev/Silence/Silence-Android
+MODULE=Silence
+MYDIR="$(dirname $(realpath $0))"
+. ${MYDIR}/envsetup.sh
 
-mkdir -p "$APP_ROOT_PATH"
-cd "$APP_ROOT_PATH"
-[ -d $MODULE ] || git clone git@github.com:SilenceIM/Silence.git $MODULE --depth 1 --branch "$VERSION" --single-branch
-[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
-if [ -d secret-keys ]; then
-	CERTS="$(pwd)"/secret-keys
-fi
+checkout
+force_java_version 12
 
-cd $MODULE
-# to create default configuration for silence
-git submodule init
-git submodule update
-
-echo "Preparing>>..$MODULE"
-
-# FIXME
-export JAVA_HOME=/usr/lib/jvm/java-12-openjdk
-
-if [ -n "$CERTS" ]; then
-	P="$(cat $CERTS/aosp/password)"
-	if ! grep -q fmo.jks build.gradle; then
-		cat >>build.gradle <<EOF
-android {
-	signingConfigs {
-		release {
-			storeFile file("$CERTS/aosp/fmo.jks")
-			storePassword "$P"
-			keyAlias "apps"
-			keyPassword "$P"
-		}
-	}
-	buildTypes {
-		release {
-			signingConfig signingConfigs.release
-		}
-	}
-}
+cat >signing.properties <<EOF
+STORE_FILE=$CERT_STORE
+STORE_PASSWORD=$CERT_PW
+KEY_ALIAS=apps
+KEY_PASSWORD=$CERT_PW
 EOF
-	fi
-	./gradlew clean assembleRelease
-	cp -f $OUTAPK $PRODUCT_OUT_PATH/$MODULE.apk
-else
-    echo "WARNING: Debug build is not supported"
-fi
+
+./gradlew clean assembleRelease
+output build/outputs/apk/release/Silence-release.apk
+cleanup
