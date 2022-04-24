3ac49a9948c294d17eaa5c2da07ff35c631a8d31
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-24 19:28:20 +0200

    Clean up lawnchair
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- lawnchair.sh
+++ lawnchair.sh
@@ -1,55 +1,23 @@
 #!/bin/sh
-MYDIR="$(dirname $(realpath $0))"
-MODULE=lawnchair
-export NEED_SRC=false
-[ -z "$ANDROID_HOME" ] && . ${MYDIR}/envsetup.sh
-[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
 [ -z "$VERSION" ] && VERSION=12-dev
+MYDIR="$(dirname $(realpath $0))"
+SOURCE=git@github.com:LawnchairLauncher/lawnchair
+. ${MYDIR}/envsetup.sh
 
-mkdir -p "$APP_ROOT_PATH"
-cd "$APP_ROOT_PATH"
-[ -d $MODULE ] || git clone --recursive git@github.com:LawnchairLauncher/$MODULE -b $VERSION --single-branch
-[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
-if [ -d secret-keys ]; then
-	CERTS="$(pwd)"/secret-keys
-fi
-
-cd $MODULE
-git submodule update --init --recursive .
+checkout
 # Permissions seem to be messed up in upstream's git repository
 chmod +x gradlew
-# change targetSdkVersion to fix crash error on 8.1.0
-#T_OLD="argetSdkVersion 27"
-#T_NEW="argetSdkVersion 22"
-#sed -i "s/$T_OLD/$T_NEW/g" app/build.gradle
+
+cat >keystore.properties <<EOF
+storeFile=$CERT_STORE
+storePassword=$CERT_PW
+keyAlias=apps
+keyPassword=$CERT_PW
+EOF
 
 # require to package with sign other than travis
-export TRAVIS_EVENT_TYPE="pull_request"
+#export TRAVIS_EVENT_TYPE="pull_request"
 
-if [ -n "$CERTS" ]; then
-	P="$(cat $CERTS/aosp/password)"
-	if ! grep -q fmo.jks build.gradle; then
-		cat >>build.gradle <<EOF
-android {
-	signingConfigs {
-		release {
-			storeFile file("$CERTS/aosp/fmo.jks")
-			storePassword "$P"
-			keyAlias "apps"
-			keyPassword "$P"
-		}
-	}
-	buildTypes {
-		release {
-			signingConfig signingConfigs.release
-		}
-	}
-}
-EOF
-	fi
-	./gradlew clean assembleLawnWithQuickstepRelease
-	cp -f ./build/outputs/apk/lawnWithQuickstep/release/Lawnchair*.apk $PRODUCT_OUT_PATH/$MODULE.apk
-else
-	./gradlew clean assembleLawnWithQuickstepDebug
-	cp -f ./build/outputs/apk/lawnWithQuickstep/debug/Lawnchair*.apk $PRODUCT_OUT_PATH/$MODULE.apk
-fi
+./gradlew assembleLawnWithQuickstepRelease
+output ./build/outputs/apk/lawnWithQuickstep/release/Lawnchair*.apk
+cleanup
