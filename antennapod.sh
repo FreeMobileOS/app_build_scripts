f723131fce2ce046775bd8b0ea7945d12ac597a2
Bernhard Rosenkränzer <bero@lindev.ch>
2022-04-24 18:29:58 +0200

    Clean up AntennaPod
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- antennapod.sh
+++ antennapod.sh
@@ -1,54 +1,23 @@
 #!/bin/sh
+[ -z "$VERSION" ] && VERSION="2.5.1"
+SOURCE=git@github.com:AntennaPod/AntennaPod
 MYDIR="$(dirname $(realpath $0))"
 . ${MYDIR}/envsetup.sh
-[ -z "$APP_ROOT_PATH" ] && APP_ROOT_PATH=$MYDIR
-[ -z "$VERSION" ] && VERSION="2.5.1"
 
-mkdir -p "$APP_ROOT_PATH"
-cd "$APP_ROOT_PATH"
-[ -d AntennaPod ] || git clone --depth 1 -b ${VERSION} git@github.com:AntennaPod/AntennaPod
-[ -d secret-keys ] || git clone git@github.com:OpenMandrivaAssociation/secret-keys
-if [ -d secret-keys ]; then
-	CERTS="$(pwd)"/secret-keys
-fi
-
-cd AntennaPod
+checkout
 
 # Use a generic name...
-sed -i -e 's,name="app_name" translate="false",name="app_name",g' core/src/main/res/values/strings.xml app/src/main/res/values/strings.xml
 autoTranslate core/src/main/res/values/strings.xml app_name Podcasts
 autoTranslate app/src/main/res/values/strings.xml app_name Podcasts
 
-export JAVA_HOME=/usr/lib/jvm/java-14-openjdk
+force_java_version 14
 
-if [ -n "$CERTS" ]; then
-	P="$(cat $CERTS/aosp/password)"
-	if ! grep -q fmo.jks app/build.gradle; then
-		cat >>app/build.gradle <<EOF
-android {
-	signingConfigs {
-		release {
-			storeFile file("$CERTS/aosp/fmo.jks")
-			storePassword "$P"
-			keyAlias "apps"
-			keyPassword "$P"
-		}
-	}
-	buildTypes {
-		release {
-			signingConfig signingConfigs.release
-		}
-	}
-}
+cat >>gradle.properties <<EOF
+releaseStoreFile=$CERT_STORE
+releaseStorePassword=$CERT_PW
+releaseKeyAlias=apps
+releaseKeyPassword=$CERT_PW
 EOF
-	fi
-	./gradlew assembleRelease
-	# Alternatively, instead of adding to build.gradle above:
-	#	$ANDROID_HOME/build-tools/26.0.0/zipalign -v -p 8 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
-	#	$ANDROID_HOME/build-tools/25.0.3/apksigner sign --ks ${CERTS}/aosp/fmo.jks --out app/build/outputs/apk/AntennaPod.apk app/build/outputs/apk/app-release-unsigned-aligned.apk
-	#	cp -f app/build/outputs/apk/AntennaPod.apk $PRODUCT_OUT_PATH
-	cp -f app/build/outputs/apk/free/release/app-free-release.apk $PRODUCT_OUT_PATH/AntennaPod.apk
-else
-	./gradlew assembleDebug
-	cp -f app/build/outputs/apk/free/debug/app-free-debug.apk $PRODUCT_OUT_PATH/AntennaPod.apk
-fi
+
+./gradlew assembleRelease
+output app/build/outputs/apk/free/release/app-free-release.apk
